7a276c8d8976c22c48ecb30c69aaa98016ea86c8
Jack <wzolleis@web.de>
2022-10-04 00:17:23 +0200

    Keine Anzeige des Users in der TrainingView. Der User ist durch den Login schon festgelegt und kann nicht geändert werden.

--- app/features/training/TrainingView.tsx
+++ app/features/training/TrainingView.tsx
@@ -1,36 +1,16 @@
 import * as React from "react";
 import messages from "~/features/i18n/messages";
 import { SerializableTraining, TrainingActionData } from "~/models/training.server";
-import { User } from "@prisma/client";
 import dateUtils from "~/dateUtils";
 
 type TrainingViewProps = {
   defaultValues?: SerializableTraining
   errors?: TrainingActionData
-  user: User
 }
 
-const TrainingView = ({ errors, defaultValues, user }: TrainingViewProps) => {
+const TrainingView = ({ errors, defaultValues }: TrainingViewProps) => {
   return (
     <>
-      <div>
-        <label htmlFor="userName" className="block mb-2 text-sm font-medium text-gray-900">
-          {messages.training.form.userName}
-        </label>
-        <input type="text"
-               id="userName"
-               name="userName"
-               required
-               disabled
-               key={user.email}
-               className="border text-sm rounded-lg block w-full p-2.5 border-gray-600 placeholder-gray-400 focus:border-blue-500 border-2"
-               defaultValue={user.email}
-        />
-        {errors?.userId ? (
-          <em className="text-red-600">{errors.userId}</em>
-        ) : null}
-      </div>
-
       <div>
         <label htmlFor="executedAt" className="block mb-2 text-sm font-medium text-gray-900">
           {messages.training.form.executedAt}
